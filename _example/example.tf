5317e893756665de34fee480b137e9791e408c66
ROHIT YADAV <rohit9950518156@gmail.com>
2024-09-08 23:28:50 +0530

    first commit

---
+++ _example/example.tf
@@ -0,0 +1,21 @@
+provider "azurerm" {
+  subscription_id = "e732de0d-fe16-4e8e-b86d-758327e0145c"
+  features {}
+}
+
+module "resource_group" {
+  source      = "git::https://github.com/SyncArcs/terraform-azure-resource-group.git?ref=v1.0.0"
+  name        = "app"
+  environment = "test"
+  location    = "North Europe"
+}
+
+
+module "vnet" {
+  source              = "./../"
+  name                = "app-vnet"
+  environment         = "test"
+  resource_group_name = module.resource_group.resource_group_name
+  location            = module.resource_group.resource_group_location
+  address_space       = "10.0.0.0/16"
+}
