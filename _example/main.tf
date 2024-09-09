6d71111bd8bf936c051f8f55dc04396c9b4527db
ROHIT YADAV <rohit9950518156@gmail.com>
2024-09-09 16:02:47 +0530

    update _example

--- _example/main.tf
+++ _example/main.tf
@@ -13,8 +13,8 @@ module "resource_group" {
 
 module "vnet" {
   source              = "./../"
-  name                = "app-vnet"
-  environment         = "test"
+  name                = "vnet"
+  environment         = "dev"
   resource_group_name = module.resource_group.resource_group_name
   location            = module.resource_group.resource_group_location
   address_space       = "10.0.0.0/16"
